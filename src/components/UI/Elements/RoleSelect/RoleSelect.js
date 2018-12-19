3e6cbc4a14cbe950e12784a3a17de9e243750349
MartonSkach <marton@secretsaucepartners.com>
2018-12-19 14:32:00 +0100

    test fix

--- src/components/UI/Elements/RoleSelect/RoleSelect.js
+++ src/components/UI/Elements/RoleSelect/RoleSelect.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
 import '../../../../styles/RoleSelect.css';
 
 class RoleSelect extends Component {
   
   handleCheckboxChange = () => {
-    console.log(this.filter)
+
   }
   render () {
     return (
@@ -44,10 +43,6 @@ class RoleSelect extends Component {
       </div>
     )
   }
-}
-
-RoleSelect.propTypes = {
-
 }
 
 export default RoleSelect
